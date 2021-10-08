3ceb62940b2ba868b01638bc2cb0252649aa29f3
KayzPart <particulier.flore@gmail.com>
2021-10-08 10:09:33 +0200

    feat: init

---
+++ app.js
@@ -0,0 +1,28 @@
+/* Découvrir le language Javascript à travers la réalisation d'un jeu, comprendre son fonctionnement en commentant le script JS */
+
+/* Présentation du jeu =>
+
+- Jouer contre l'ordinateur afin de trouver un nombre aléatoire entre 0 et 100
+
+- Il y a un système de vie représenter par des coeurs (5 essais)
+
+- Un sytème de chaud/froid qui va indiquer au joeur si il se raproche ou non de la valeur à trouver 
+
+- Nous manipulons donc le DOM en JS */
+
+/* --- Nous décomposons la création de ce jeu en 8 étapes ---
+
+- 1) = La mise en place
+    => Editeur de code utiliser : Visual Studio Code avec les extensions : Color Highlight, Es Lint (raccourcies de commandes JS = écrire plus vite avec moins d'erreur) et Live Server
+
+- 2) = HTML
+- 3) = CSS
+
+- 4) = Début JS
+- 5) = Logique globale du jeu
+- 6) = Système chaud/froid
+- 7) = Système de vies
+- 8) = Bouton recommencer
+
+*/
+
